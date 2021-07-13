ba624947183ca8bef12e3536c33dcf38c03fe0e9
cristianfelipecalderonfonseca <c.calderon.fonseca@accenture.com>
2021-07-13 10:24:32 -0600

    Have added repo to Github, first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,80 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import {CountdownComponent} from 'ngx-countdown';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'timeController';
+  colors = ["button-blue","button-orange","button-red","button-yellow","button-green","button-pink"];
+  gameReference: any = "";
+  initialTimeConfig: object = {'leftTime': 5, 'demand': true};
+  runningTime: boolean = false;
+  matchCompleted: boolean = false;
+  playerToken: string = '';
+
+  @ViewChild('playerOne', { static: false }) private countdownPlayerOne: any = CountdownComponent;
+  @ViewChild('playerTwo', { static: false }) private countdownPlayerTwo: any = CountdownComponent;
+
+  ngOnInit(): void {}
+
+  changeColor(color: string, player: string) {
+    if (player === "second-player") {
+      const getterSecondPlayerController = document.getElementById('playerTwo-getter-js');
+      getterSecondPlayerController?.setAttribute('class', `player player-two ${color}`);
+    } else if (player === "first-player") {
+      const getterSecondPlayerController = document.getElementById('playerOne-getter-js');
+      getterSecondPlayerController?.setAttribute('class', `player player-one ${color}`);
+    }
+  }
+
+  startTimer(roundPlayer: any): void {
+    if (!this.matchCompleted) {
+      this.runningTime = true;
+      if (roundPlayer === 'first-player') {
+        this.countdownPlayerTwo.pause();
+        this.countdownPlayerOne.begin();
+        this.playerToken = roundPlayer;
+      } else if (roundPlayer === 'second-player') {
+        this.countdownPlayerOne.pause();
+        this.countdownPlayerTwo.begin();
+        this.playerToken = roundPlayer;
+      }
+    }
+  }
+
+  restartTimer(): void {
+    const getterFirstPlayerController = document.getElementById('playerOne-getter-js');
+    const getterSecondPlayerController = document.getElementById('playerTwo-getter-js');
+    this.runningTime = false;
+    this.matchCompleted = false;
+    this.countdownPlayerOne.restart();
+    this.countdownPlayerTwo.restart();
+
+    getterSecondPlayerController?.setAttribute('class', 'player player-two' );
+    getterFirstPlayerController?.setAttribute('class', 'player player-one');
+  }
+
+  pauseTimer(): void {
+    this.runningTime = false;
+    this.countdownPlayerOne.pause();
+    this.countdownPlayerTwo.pause();
+  }
+
+  checkTimesUp($event: any, player: string) {
+    if ($event.action === 'done') {
+      this.runningTime = false;
+      this.matchCompleted = true;
+
+      if (player === "second-player") {
+        const getterSecondPlayerController = document.getElementById('playerTwo-getter-js');
+        getterSecondPlayerController?.setAttribute('class', 'player player-two finished' );
+      } else if (player === "first-player") {
+        const getterFirstPlayerController = document.getElementById('playerOne-getter-js');
+        getterFirstPlayerController?.setAttribute('class', 'player player-one finished');
+      }
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CountdownModule } from 'ngx-countdown';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +9,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CountdownModule
   ],
   providers: [],
   bootstrap: [AppComponent]
